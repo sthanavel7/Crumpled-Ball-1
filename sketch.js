a1a237762ae346fe569ff61f4985dac218b86634
sthanavel7 <57311048+sthanavel7@users.noreply.github.com>
2020-12-13 20:42:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground,engine, world,dustbin,paper;
+function setup() {
+  createCanvas(800, 400);
+  rectMode(CENTER);
+
+  
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
+}
+
+function draw() {
+  
+    background("black");
+    textSize(20);
+    fill("red");
+    text("   Press up  arrow to throw away the trash.", 50, 200)
+     keyCode === UP_ARROW
+      gameState = "play"
+    
+  
+  
+    rectMode(CENTER);
+    background(0);
+    dustbin.display();
+    paper.display();
+
+  
+}
+
+
+function keyPressed(){
+  if (keyCode === UP_ARROW ) {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 13,
+      y: -12
+    });
+  }
+}
